2f841ad257cb53265eda5eb041d5190571abe800
董聿凌 <gb2013429@gmail.com>
2022-02-26 16:45:00 +0800

    logout

--- js/login.js
+++ js/login.js
@@ -1,7 +1,7 @@
 import { createApp } from 'https://cdnjs.cloudflare.com/ajax/libs/vue/3.2.29/vue.esm-browser.min.js';
 
 
-//燈入及登入狀態、取得產品列表
+//登入及登入狀態、取得產品列表
 const site='https://vue3-course-api.hexschool.io/v2';
 const path='yuling202202';
 
@@ -34,7 +34,28 @@ const app = createApp({
                     alert('請填寫正確的帳號及密碼');
                 }
             })
-        }
+        },
+        logout(){
+            const url =`${site}/logput`;
+            axios.post(url,this.user)
+            .then((res) =>{
+                console.log(res)
+                const {token ,expired} =res.data;
+                console.log(token,expired)
+                document.cookie =`hexToken=${token}; expires=${new Date(expired)};`;
+                window.location='https://ginatung.github.io/2022Vue-W4/week4';
+                if(res.data.message="登出成功"){
+                    alert('登出成功');
+                }
+            })
+            // 登入帳號密碼輸入錯誤時，可以加入 alert 彈跳視窗來顯示登入失敗訊息。
+            .catch((error)=>{
+                console.log(error.data.message)
+                if(error.data.message="登出失敗"){
+                    alert('請填寫正確的帳號及密碼');
+                }
+            })
+        },
     }
 })
 app.mount('#app');
