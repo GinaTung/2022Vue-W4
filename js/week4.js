ec9ddf718d5c921172fa2a266b89ac477f74773d
董聿凌 <gb2013429@gmail.com>
2022-03-05 14:31:41 +0800

    del

--- js/week4.js
+++ js/week4.js
@@ -121,7 +121,7 @@ app.component('delProductModal',{
     props: ['item'],
     methods:{
         delProduct(){
-            let url =`${site}/api/${api_path}/admin/product/${this.tempProduct.id}`;
+            let url =`${site}/api/${api_path}/admin/product/${this.item.id}`;
 
             axios.delete(url)
             .then((res)=>{
